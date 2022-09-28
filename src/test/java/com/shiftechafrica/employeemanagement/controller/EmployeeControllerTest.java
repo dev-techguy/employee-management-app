629ec77184c3f8a6e6d4828d9d887093230728fa
dev-techguy <dev.techguy@gmail.com>
2022-09-28 18:48:48 +0300

    :white_check_mark: Added tests.

---
+++ src/test/java/com/shiftechafrica/employeemanagement/controller/EmployeeControllerTest.java
@@ -0,0 +1,27 @@
+package com.shiftechafrica.employeemanagement.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class EmployeeControllerTest {
+
+    @Test
+    void getAllEmployees() {
+    }
+
+    @Test
+    void getEmployeeId() {
+    }
+
+    @Test
+    void addEmployee() {
+    }
+
+    @Test
+    void updateEmployee() {
+    }
+
+    @Test
+    void deleteEmployee() {
+    }
+}
