90e2262675f5d3e5227190bc4bc8af4f0b5f4cf3
dev-techguy <dev.techguy@gmail.com>
2022-09-28 18:50:01 +0300

    :recycle: Refactoring code.

--- src/main/java/com/shiftechafrica/employeemanagement/service/EmployeeService.java
+++ src/main/java/com/shiftechafrica/employeemanagement/service/EmployeeService.java
@@ -34,7 +34,7 @@ public class EmployeeService {
                 .orElseThrow(() -> new UserNotFoundException("User by id " + id + " was not found"));
     }
 
-    public void deleteEmployee(Long id){
+    public void deleteEmployee(Long id) {
         employeeRepo.deleteEmployeeById(id);
     }
 }
